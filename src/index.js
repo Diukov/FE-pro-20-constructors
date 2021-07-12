f001e407531c349b664456b31f75eb39b37768f0
Dmitriy Diukov <diukovdmitriy@gmail.com>
2021-07-12 20:07:24 +0300

    Student's constructor task

--- src/index.js
+++ src/index.js
@@ -1,91 +1,96 @@
 function Student(name, surname, bdayYear) {
-  this.name = name;
-  this.surname = surname;
-  this["Year of birth"] = bdayYear;
-  this["Student's attedence"] = new Array(10);
-  this["Student's marks"] = new Array(10);
+    const maxAttedence = 10;
+    const maxRatings = 10;
+    const passingScoreMarks = 9;
+    const passingScoreAttedence = 0.9;
+    let attendenceCounter = 0;
+    let marksCounter = 0;
 
-  let attendenceCounter = 0;
-  let marksCounter = 0;
+    this.name = name;
+    this.surname = surname;
+    this['Year of birth'] = bdayYear;
+    this['Students attedence'] = new Array(maxAttedence);
+    this['Students marks'] = new Array(maxRatings);
 
-  this.age = function () {
-    let currentYear = new Date().getFullYear();
-    let studentsAge = currentYear - this["Year of birth"];
+    this.age = function () {
+        const currentYear = new Date().getFullYear();
+        const studentsAge = currentYear - Number(this['Year of birth']);
 
-    return studentsAge;
-  };
-
-  this.totalAverage = function () {
-    let averageMarks = 0;
-
-    for (let key in this["Student's marks"]) {
-      averageMarks += this["Student's marks"][key];
-    }
-
-    return averageMarks /= marksCounter;
-  };
-
-  this.present = function () {
-    try {
-      if (attendenceCounter < 10) {
-        this["Student's attedence"][attendenceCounter] = true;
-        attendenceCounter++;
-      } else throw new Error("невозможно отметить посещение - список переполнен.")
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  this.absent = function () {
-    try {
-      if (attendenceCounter < 10) {
-        this["Student's attedence"][attendenceCounter] = false;
-        attendenceCounter++;
-      } else throw new Error("невозможно отметить посещение - список переполнен.")
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  this.mark = function (mark) {
-    try {
-      if (marksCounter < 10) {
-        this["Student's marks"][marksCounter] = mark;
-        marksCounter++;
-      } else throw new Error("невозможно поставить оценку - список переполнен.")
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  this.summary = function () {
-    let result = {
-      average: 0,
-      attendence: 0
+        return studentsAge;
     };
 
-    for (let counter = 0; counter < marksCounter; counter++) {
-      result.average += this["Student's marks"][counter];
-    }
+    this.totalAverage = function () {
+        let averageMarks = 0;
 
-    result.average /= marksCounter;
+        for (const key in this['Students marks']) {
+            averageMarks += this['Students marks'][key];
+        }
 
-    for (let counter = 0; counter < attendenceCounter; counter++) {
-      if (this["Student's attedence"][counter]) {
-        result.attendence++;
-      }
-    }
+        return averageMarks /= marksCounter;
+    };
 
-    result.attendence /= attendenceCounter;
+    this.present = function () {
+        try {
+            if (attendenceCounter < maxAttedence) {
+                this['Students attedence'][attendenceCounter] = true;
+                attendenceCounter++;
+            } else throw new Error('невозможно отметить посещение - список переполнен.');
+        } catch (error) {
+            alert(error);
+        }
+    };
 
-    if (result.average > 9 && result.attendence > 0.9) {
-      return "Ути какой молодчинка!";
-    } else if ((result.average < 9 && result.attendence > 0.9) || (result.average > 9 && result.attendence < 0.9)) {
-      return "Норм, но можно лучше.";
-    } else return "Редиска!";
-  };
+    this.absent = function () {
+        try {
+            if (attendenceCounter < maxAttedence) {
+                this['Students attedence'][attendenceCounter] = false;
+                attendenceCounter++;
+            } else throw new Error('невозможно отметить посещение - список переполнен.');
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    this.mark = function (mark) {
+        try {
+            if (marksCounter < maxRatings) {
+                this['Students marks'][marksCounter] = mark;
+                marksCounter++;
+            } else throw new Error('невозможно поставить оценку - список переполнен.');
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    this.summary = function () {
+        const result = {
+            average: 0,
+            attendence: 0
+        };
+
+        for (let counter = 0; counter < marksCounter; counter++) {
+            result.average += this['Students marks'][counter];
+        }
+
+        result.average /= marksCounter;
+
+        for (let counter = 0; counter < attendenceCounter; counter++) {
+            if (this['Students attedence'][counter]) {
+                result.attendence++;
+            }
+        }
+
+        result.attendence /= attendenceCounter;
+
+        if (result.average > passingScoreMarks && result.attendence > passingScoreAttedence) {
+            return 'Ути какой молодчинка!';
+        } else if ((result.average < passingScoreMarks && result.attendence > passingScoreAttedence) ||
+            (result.average > passingScoreMarks && result.attendence < passingScoreAttedence)) {
+            return 'Норм, но можно лучше.';
+        } else return 'Редиска!';
+    };
 }
 
-const student = new Student("Vasya", "Petrov", 1994);
+const student = new Student('Vasya', 'Petrov', '1994');
 
 student.present();
